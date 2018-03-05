6d0d0f820f509e319c88c6f861b21f023cdbcf89
jrn011 <jrn011@bucknell.edu>
2018-03-05 12:45:37 -0500

    update

--- AJAXpractice/script.js
+++ AJAXpractice/script.js
@@ -8,3 +8,12 @@ fetch('https://dog.ceo/api/breeds/image/random')
     var myImage = document.getElementById('rand');
     myImage.src=myJson.message;
   });
+
+
+fetch('https://dog.ceo/api/breeds/list/all')
+	.then(function(response){
+		console.log(response)});
+
+
+var arr= fetch('https://dog.ceo/api/breeds/list').then(resp => resp.json()).then(j => return j.message);
+console.log(arr);
