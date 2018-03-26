0fe0f6f55668013072641989c070e3567b21e0c1
jrn011 <jrn011@bucknell.edu>
2018-03-26 19:34:06 -0400

    project 2 progress

--- project2/src/index.js
+++ project2/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import DogBreedsSelect from './t';
-import CourseDisplay from './t2';
+import CourseDisplay from './proj2';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
