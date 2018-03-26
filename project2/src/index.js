04bb76901ac4ce97b396f982f52ec55208ae6087
jrn011 <jrn011@bucknell.edu>
2018-03-25 21:48:40 -0400

    Proj 2 progress

---
+++ project2/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import DogBreedImgSelect from './t';
+import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(<DogBreedImgSelect />, document.getElementById('root'));
+registerServiceWorker();
